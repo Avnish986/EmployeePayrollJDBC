7c29ad591a5d44ffa40fd72d7449cebb6c678805
avnish986 <avnishgupta986@gmail.com>
2020-10-30 16:52:34 +0530

    [Avnish]Added: retrieve average salary of employee

--- src/test/java/com/sql/demo/EmployeePayrollTest.java
+++ src/test/java/com/sql/demo/EmployeePayrollTest.java
@@ -2,6 +2,7 @@ package com.sql.demo;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -44,6 +45,15 @@ public class EmployeePayrollTest {
 		List<EmployeePayrollData> employeePayrollData = service
 				.readEmployeeDetailsForDateRange(IOService.DB_IO, startDate, endDate);
 		Assert.assertEquals(4, employeePayrollData.size());
+	}
+	
+	@Test
+	public void givenEmployeePayrollData_WhenRetrievedAvg_SalaryByGender_ShouldReturnValuesAsExpected() {
+		EmployeePayrollService service = new EmployeePayrollService();
+		service.readEmployeePayrollData(IOService.DB_IO);
+		Map<String, Double> averageSalaryByGender = service.readAverageSalaryByGender(IOService.DB_IO);
+		Assert.assertTrue(
+				averageSalaryByGender.get("M").equals(200000.0) && averageSalaryByGender.get("F").equals(2000000.0));
 	}
 
 }
