259e7743efe8381807f0f1fa9d13daece629e2fe
avnish986 <avnishgupta986@gmail.com>
2020-10-30 15:46:45 +0530

    [Avnish]Added: using update statement update salary of employee

--- src/main/java/com/sql/demo/EmployeePayrollData.java
+++ src/main/java/com/sql/demo/EmployeePayrollData.java
@@ -8,6 +8,12 @@ public class EmployeePayrollData {
 	public double basic_pay;
 	public LocalDate start;
 
+	public EmployeePayrollData(int id, String name, double basic_pay) {
+		this.id = id;
+		this.name = name;
+		this.basic_pay = basic_pay;
+	}
+
 	public EmployeePayrollData(int id, String name, double basic_pay, LocalDate start) {
 		this.id = id;
 		this.name = name;
@@ -17,7 +23,17 @@ public class EmployeePayrollData {
 
 	@Override
 	public String toString() {
-		return " id: " + id + " name: " + name + " basic_pay: " + basic_pay + " startdate: " + start;
+		return "id=" + id + ", name=" + name + ", salary=" + basic_pay+"; ";
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		EmployeePayrollData that = (EmployeePayrollData) o;
+		return id == that.id && Double.compare(that.basic_pay, basic_pay) == 0 && name.equals(that.name);
 	}
 
 }
