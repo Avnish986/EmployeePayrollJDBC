c712ed5978e25b45d7fed00256987fe5ef4390c2
avnish986 <avnishgupta986@gmail.com>
2020-11-02 16:13:08 +0530

    [Avnish]Added: getting instance of adding details of employee in employeepayroll without thread

--- src/main/java/com/sql/demo/EmployeePayrollData.java
+++ src/main/java/com/sql/demo/EmployeePayrollData.java
@@ -7,7 +7,9 @@ public class EmployeePayrollData {
 	public String name;
 	public double basic_pay;
 	public LocalDate start;
-
+	public String gender;
+	public String department;
+	
 	public EmployeePayrollData(int id, String name, double basic_pay) {
 		this.id = id;
 		this.name = name;
@@ -19,6 +21,13 @@ public class EmployeePayrollData {
 		this.name = name;
 		this.basic_pay = basic_pay;
 		this.start = start;
+	}
+	
+	public EmployeePayrollData(int id, String name, double salary, LocalDate startDate,String gender,String department) {
+		this(id, name, salary);
+		this.start = startDate;
+		this.gender=gender;
+		this.department=department;
 	}
 
 	@Override
@@ -35,5 +44,7 @@ public class EmployeePayrollData {
 		EmployeePayrollData that = (EmployeePayrollData) o;
 		return id == that.id && Double.compare(that.basic_pay, basic_pay) == 0 && name.equals(that.name);
 	}
+	
+	
 
 }
